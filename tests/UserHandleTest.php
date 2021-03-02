2da4a495fbd7ad99940ef1ca23b19799a4ff9b87
ubuntu-vm <1174865138@qq.com>
2021-03-03 02:34:05 +0800

    s

--- tests/UserHandleTest.php
+++ tests/UserHandleTest.php
@@ -24,17 +24,17 @@ class UserHandleTest extends TestCase
     public function testHandle(array $data): void
     {
         // 在类UserHandle下模拟一个getUsersByJson的方法,并设定返回值
-        // $mock = $this->getMockBuilder(UserHandle::class)->setMethods(['getUsersByJson'])->getMock();
-        // $mock->expects($this->once())->method('getUsersByJson')->willReturn(reset($data));
-        // $logs = $mock->handle();
+        ini_set('pdo_mysql.default_socket', '/var/run/mysqld/mysqld.sock');
+        var_dump(ini_get('pdo_mysql.default_socket'));
+        $mock = $this->getMockBuilder(UserHandle::class)->setMethods(['getUsersByJson'])->getMock();
+        $mock->expects($this->once())->method('getUsersByJson')->willReturn(reset($data));
+        $logs = $mock->handle();
 
-        // $this->assertStringStartsWith(key($data), $logs[0]);
+        $this->assertStringStartsWith(key($data), $logs[0]);
 
 
-        var_dump(ini_get('pdo_mysql.default_socket'));exit();
-
-        $s = $data;
-        $this->assertStringStartsWith('a', 'aa');
+        // $s = $data;
+        // $this->assertStringStartsWith('a', 'aa');
     }
 
 
