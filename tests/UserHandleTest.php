66b4025dea779f0b6e96bf108fc5c35d0b25e1e6
ubuntu-vm <1174865138@qq.com>
2021-03-05 19:04:15 +0800

    s

--- tests/UserHandleTest.php
+++ tests/UserHandleTest.php
@@ -17,78 +17,23 @@ class UserHandleTest extends TestCase
     }
 
     /**
-     * @dataProvider dataHandle
      * @covers Library\UserHandle::handle
      * @todo   Implement testHandle().
      */
     public function testHandle(array $data): void
     {
         // 在类UserHandle下模拟一个getUsersByJson的方法,并设定返回值
-        $mock = $this->getMockBuilder(UserHandle::class)->setMethods(['getUsersByJson'])->getMock();
-        $mock->expects($this->once())->method('getUsersByJson')->willReturn(reset($data));
-        $logs = $mock->handle();
+        // $mock = $this->getMockBuilder(UserHandle::class)->setMethods(['getUsersByJson'])->getMock();
+        // $mock->expects($this->once())->method('getUsersByJson')->willReturn(reset($data));
+        // $logs = $mock->handle();
 
-        $this->assertStringStartsWith(key($data), $logs[0]);
+        // $this->assertStringStartsWith(key($data), $logs[0]);
 
 
-        // $s = $data;
-        // $this->assertStringStartsWith('a', 'aa');
+        $s = $data;
+        $this->assertStringStartsWith('a', 'aa');
     }
 
-
-    public function dataHandle(): array
-    {
-        return [
-            [[
-                'add' => [
-                    [
-                        "username" => "test",
-                        "password" => "90a3ed9e32b2aaf4c61c410eb925426119e1a9dc53d4286ade99a809",
-                        "quota" => 107374182400,
-                        "enable" => true,
-                        "level" => 0,
-                        "expiryDate" => "2050-01-01",
-                        "passwordShow" => "dGVzdA==",
-                    ],
-                    [
-                        "username" => "test2",
-                        "password" => "90a3ed9e32b2aaf4c61c410eb925426119e1a9dc53d4286ade99a809",
-                        "quota" => 107374182400,
-                        "enable" => true,
-                        "level" => 0,
-                        "expiryDate" => "2050-01-01",
-                        "passwordShow" => "dGVzdA==",
-                    ],
-                ],
-            ]],
-            [[
-                'del' => [
-                    [
-                        "username" => "test",
-                        "password" => "90a3ed9e32b2aaf4c61c410eb925426119e1a9dc53d4286ade99a809",
-                        "quota" => 107374182400,
-                        "enable" => true,
-                        "level" => 0,
-                        "expiryDate" => "2050-01-01",
-                        "passwordShow" => "dGVzdA==",
-                    ],
-                ],
-            ]],
-            [[
-                'update' => [
-                    [
-                        "username" => "test",
-                        "password" => "90a3ed9e32b2aaf4c61c410eb925426119e1a9dc53d4286ade99a809",
-                        "quota" => 107374182400,
-                        "enable" => true,
-                        "level" => 0,
-                        "expiryDate" => "2050-01-02",
-                        "passwordShow" => "dGVzdA==",
-                    ],
-                ],
-            ]],
-        ];
-    }
 
 
 }
